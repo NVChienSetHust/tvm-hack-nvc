09170d0b188d0a22eb3a2fde8c10b3e91d7de553
NVChienSetHust <chien.nv.set@gmail.com>
2024-11-04 09:51:28 +0100

    build config for build conda environment

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -133,7 +133,7 @@ set(USE_MICRO_STANDALONE_RUNTIME OFF)
 # - OFF: disable llvm, note this will disable CPU codegen
 #        which is needed for most cases
 # - /path/to/llvm-config: enable specific LLVM when multiple llvm-dev is available.
-set(USE_LLVM OFF)
+set(USE_LLVM ON)
 
 #---------------------------------------------
 # Contrib libraries
@@ -270,7 +270,7 @@ set(USE_VERILATOR OFF)
 set(USE_ANTLR OFF)
 
 # Whether use Relay debug mode
-set(USE_RELAY_DEBUG OFF)
+set(USE_RELAY_DEBUG ON)
 
 # Whether to build fast VTA simulator driver
 set(USE_VTA_FSIM OFF)
